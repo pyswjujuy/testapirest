451dc74aa55f9282dcbfb3a2846d58ba76c81c40
arespinoza <alfredo_espi@hotmail.com>
2025-10-27 00:16:42 -0300

    subinedo

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Definimos una ruta GET
 app.get('/', (req, res) => {
-  res.json({ mensaje: 'Hola HIA 3000!!!' });
+  res.json({ mensaje: 'Hola HIA 4000!!!' });
 });
 
 // Iniciamos el servidor
